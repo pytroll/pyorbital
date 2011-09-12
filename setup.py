f8d67e197431b76fbf9356214d5c8632920eaac6
Martin Raspaud <martin.raspaud@smhi.se>
2011-09-12 07:42:04 +0200

    Made the package more package-like.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2011 SMHI
+
+# Author(s):
+
+#   Martin Raspaud <martin.raspaud@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(name='pyorbital',
+      version="0.1",
+      description='Orbital parameters and astronomical computations in Python',
+      author='Martin Raspaud',
+      author_email='martin.raspaud@smhi.se',
+      package_dir = {'pyorbital': 'pyorbital'},
+      packages = ['pyorbital'],      
+      install_requires=['numpy']
+      )
+
+
+

9f096c3183574376eab698e9e589de36f3e38f23
Martin Raspaud <martin.raspaud@smhi.se>
2011-09-30 15:47:48 +0200

    Now using unittest module for aiaa test cases.

--- pyorbital/test_aiaa.py
+++ pyorbital/test_aiaa.py
@@ -29,6 +29,7 @@ from pyorbital.orbital import Orbital, OrbitElements, _SGDP4
 from pyorbital import tlefile
 import numpy as np
 from datetime import timedelta, datetime
+import unittest
 
 class LineOrbital(Orbital):
     """Read TLE lines instead of file.
@@ -67,50 +68,57 @@ def get_results(satnumber, delay):
                         float(sline[6]),
                         utc_time)
             line = f_2.readline()
-                              
-                
+
+class AIAAIntegrationTest(unittest.TestCase):
+    """Test against the AIAA test cases.
+    """
+    
+    def test_aiaa(self):
+        """Do the tests against AIAA test cases.
+        """
+        with open("SGP4-VER.TLE") as f__:
+            test_line = f__.readline()
+            while(test_line):
+                if test_line.startswith("#"):
+                    pass
+                if test_line.startswith("1 "):
+                    line1 = test_line
+                if test_line.startswith("2 "):
+                    line2 = test_line[:69]
+                    times = str.split(test_line[69:])
+                    times = np.arange(float(times[0]),
+                                      float(times[1]) + 1,
+                                      float(times[2]))
+                    try:
+                        o = LineOrbital("unknown", line1, line2)
+                    except:
+                        # WARNING: skipping deep space computations
+                        test_line = f__.readline()
+                        continue
+                    for delay in times:
+                        try:
+                            test_time = timedelta(minutes=delay) + o.tle.epoch
+                            pos, vel = o.get_position(test_time, False)
+                            res = get_results(int(o.tle.satnumber), float(delay))
+                        except:
+                            # WARNING: TODO
+                            break
+
+                        delta_pos = 5e-6 # km =  5 mm
+                        delta_vel = 5e-9 # km/s = 5 um/s
+                        delta_time = 50 # microseconds
+                        self.assertTrue(abs(res[0] - pos[0]) < delta_pos)
+                        self.assertTrue(abs(res[1] - pos[1]) < delta_pos)
+                        self.assertTrue(abs(res[2] - pos[2]) < delta_pos)
+                        self.assertTrue(abs(res[3] - vel[0]) < delta_vel)
+                        self.assertTrue(abs(res[4] - vel[1]) < delta_vel)
+                        self.assertTrue(abs(res[5] - vel[2]) < delta_vel)
+                        if res[6] is not None:
+                            self.assertTrue(abs((res[6] - test_time).microseconds)
+                                            < delta_time)
+                        
+                test_line = f__.readline()
+        
 
 if __name__ == '__main__':
-    with open("SGP4-VER.TLE") as f__:
-        test_line = f__.readline()
-        while(test_line):
-            if test_line.startswith("#"):
-                pass
-            if test_line.startswith("1 "):
-                line1 = test_line
-            if test_line.startswith("2 "):
-                line2 = test_line[:69]
-                times = str.split(test_line[69:])
-                times = np.arange(float(times[0]),
-                                  float(times[1]) + 1,
-                                  float(times[2]))
-                try:
-                    o = LineOrbital("unknown", line1, line2)
-                except:
-                    print "WARNING: skipping deep space computations"
-                    test_line = f__.readline()
-                    continue
-                print "*" * 80
-                print "satnumber:", o.tle.satnumber
-                for delay in times:
-                    try:
-                        test_time = timedelta(minutes=delay) + o.tle.epoch
-                        pos, vel = o.get_position(test_time, False)
-                        res = get_results(int(o.tle.satnumber), float(delay))
-                    except:
-                        print "WARNING: TODO"
-                        break
-
-                    print "delay:" , delay, "minutes"
-                    print "shown: pos_x(km) pos_y(km) pos_z(km) vel_x(km/s) vel_y(km/s) vel_z(km/s) time"
-                    print "expected:"
-                    print res[0], res[1], res[2], res[3], res[4], res[5], res[6]
-                    print "got:"
-                    print pos[0], pos[1], pos[2], vel[0], vel[1], vel[2], test_time
-                    print "difference (expected-got):"
-                    if res[6] is not None:
-                        print res[0] - pos[0], res[1] - pos[1], res[2] - pos[2], res[3] - vel[0], res[4] - vel[1], res[5] - vel[2], (res[6] - test_time).microseconds
-                    else:
-                        print res[0] - pos[0], res[1] - pos[1], res[2] - pos[2], res[3] - vel[0], res[4] - vel[1], res[5] - vel[2]
-                    print "-" * 80
-            test_line = f__.readline()
+    unittest.main()

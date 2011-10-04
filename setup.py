6105091d8008f8b7551cb7ab9d5e28106d2cbaf4
Martin Raspaud <martin.raspaud@smhi.se>
2011-10-04 17:44:53 +0200

    Bumped version number to 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from setuptools import setup
 
 setup(name='pyorbital',
-      version="0.1",
+      version="0.2.0",
       description='Orbital parameters and astronomical computations in Python',
       author='Martin Raspaud, Esben S. Nielsen',
       author_email='martin.raspaud@smhi.se, esn@dmi.dk',

503bbbbceb1d7ef80bcf54f6821343ab1b57322a
Esben S. Nielsen <esn@dmi.dk>
2013-03-06 11:38:14 +0100

    bumped version number and marked as stable

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@
 from setuptools import setup
 
 setup(name='pyorbital',
-      version="v0.2.1",
+      version="v0.2.2",
       description='Orbital parameters and astronomical computations in Python',
       author='Martin Raspaud, Esben S. Nielsen',
       author_email='martin.raspaud@smhi.se, esn@dmi.dk',
-      classifiers=["Development Status :: 4 - Beta",
+      classifiers=["Development Status :: 5 - Production/Stable",
                    "Intended Audience :: Science/Research",
                    "License :: OSI Approved :: GNU General Public License v3 " +
                    "or later (GPLv3+)",

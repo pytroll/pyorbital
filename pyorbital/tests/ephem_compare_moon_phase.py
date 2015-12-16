6f7068daf9959695a1f72818ce4ecda427e467c3
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2015-12-16 23:51:15 +0100

    Add header to the two test programs for moon position and phase
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- pyorbital/tests/ephem_compare_moon_phase.py
+++ pyorbital/tests/ephem_compare_moon_phase.py
@@ -1,8 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Adam.Dybbroe
+
+# Author(s):
+
+#   Adam.Dybbroe <adam.dybbroe@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Compare accuracy of the moon_phase function with the ephem library"""
 
 import ephem
 import datetime
-import moon_phase
+from pyorbital.moon_phase import moon_phase
 import numpy as np
 
 start_time = datetime.datetime(2011, 12, 1, hour=12)
@@ -20,12 +42,11 @@ for idx in range(1000):
     norrk.date = time_t.strftime('%Y/%m/%d %H:%M')
     m = ephem.Moon(norrk)
     ephem_phase = m.phase
-    phase = moon_phase.moon_phase(time_t)
-
+    phase = moon_phase(time_t)
 
     #diff = 100*phase - ephem_phase
     tlist.append(time_t)
-    #print time_t.strftime("%Y-%m-%d %H:%M"), diff
+    # print time_t.strftime("%Y-%m-%d %H:%M"), diff
     ephem_pha.append(ephem_phase)
     pha.append(phase)
     time_t = time_t + delta_t
@@ -43,7 +64,7 @@ ax = fig.add_subplot(111)
 ax.scatter(ephem_pha, ephem_pha - pha)
 
 ax.set_xlabel('Ephem moon phase (%)', fontsize=20)
-ax.set_ylabel('Difference - Ephem-myprogam (%)', fontsize=20)
+ax.set_ylabel('Difference - Ephem-pyorbital (%)', fontsize=20)
 ax.set_title('Comparing Moon phase derivation')
 ax.grid(True)
 
--- pyorbital/tests/ephem_compare_moon_position.py
+++ pyorbital/tests/ephem_compare_moon_position.py
@@ -1,17 +1,41 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Adam.Dybbroe
+
+# Author(s):
+
+#   Adam.Dybbroe <adam.dybbroe@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Compare my derivation of the altitude and azimuth of the moon with that
-derived from ephem"""
+derived from ephem
+
+"""
 
 import ephem
 import datetime
 import numpy as np
-import planets
+from pyorbital import planets
 
 if __name__ == "__main__":
 
     # Norrköping: Lat N 58° 35′ 15″ Lon E 16° 11′ 15″
-    llon = 16 + 11.0/60. + 15./3600.
-    llat = 58. + 35./60. + 15./3600.
+    llon = 16 + 11.0 / 60. + 15. / 3600.
+    llat = 58. + 35. / 60. + 15. / 3600.
 
     currtime = datetime.datetime(2012, 1, 7, 12, 0)
     endtime = datetime.datetime(2012, 1, 8, 12, 0)
@@ -37,15 +61,14 @@ if __name__ == "__main__":
 
         moon = planets.Moon(currtime)
         rasc, decl, alt, azi = moon.topocentric_position(llon, llat)
-        #azi, alt, ha_ = planets.azimuthal_coord(currtime, 
-        #                                        moon.lonsun, 
+        # azi, alt, ha_ = planets.azimuthal_coord(currtime,
+        #                                        moon.lonsun,
         #                                        llon, llat, rasc, decl)
         heights.append(alt)
         azimuths.append(azi)
         dtimes.append(currtime)
         currtime = currtime + delta_t
 
-    
     heights = np.array(heights)
     ephem_heights = np.array(ephem_heights)
     azimuths = np.array(azimuths)
@@ -55,25 +78,24 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     fig = plt.figure()
-    
+
     ax = fig.add_subplot(111)
     ax.scatter(ephem_heights, ephem_heights - heights)
 
     ax.set_xlabel('Ephem moon height (deg)', fontsize=20)
-    ax.set_ylabel('Difference - Ephem-myprogam (deg)', fontsize=20)
+    ax.set_ylabel('Difference - Ephem-pyorbital (deg)', fontsize=20)
     ax.set_title('Comparing Moon height derivation')
     ax.grid(True)
-    
+
     plt.savefig('./moonheight_compare.png')
 
-
-    fig = plt.figure()    
+    fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(ephem_azimuths, ephem_azimuths - azimuths)
 
     ax.set_xlabel('Ephem moon azimuth (deg)', fontsize=20)
-    ax.set_ylabel('Difference - Ephem-myprogam (deg)', fontsize=20)
+    ax.set_ylabel('Difference - Ephem-pyorbital (deg)', fontsize=20)
     ax.set_title('Comparing Moon azimuth derivation')
     ax.grid(True)
-    
+
     plt.savefig('./moonazimuth_compare.png')

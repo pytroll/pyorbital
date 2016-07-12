943ae4682310e917cc5a17b89e2304d67f8bc3e1
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-07-13 00:45:34 +0200

    Make Julian function handle arrays/sequences
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- pyorbital/moon_phase.py
+++ pyorbital/moon_phase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2013 Adam.Dybbroe
+# Copyright (c) 2013, 2016 Adam.Dybbroe
 
 # Author(s):
 
@@ -24,10 +24,48 @@
 """
 
 from numpy import deg2rad, sin, cos, tan, arctan, fabs
+import numpy as np
 
 import math
 PI = math.pi
 SMALL_FLOAT = 1.0e-12
+
+
+def seqJulian(dobj):
+    """Returns the number of julian days for the specified date-time.
+    Input = datetime object.
+    """
+    import collections
+
+    if type(dobj) == 'datetime.datetime':
+        dobj = np.array([dobj], 'datetime64[ms]')
+    elif isinstance(dobj, np.ndarray):
+        dobj = dobj.astype('datetime64[ms]')
+    elif isinstance(dobj, collections.Sequence):
+        dobj = np.array(dobj, 'datetime64[ms]')
+
+    day = (
+        dobj - dobj.astype('datetime64[M]')).astype('f') / (24. * 3600. * 1000) + 1.0
+    month = (
+        dobj.astype('datetime64[M]') - dobj.astype('datetime64[Y]')).astype('f') + 1
+    year = dobj.astype('datetime64[Y]').astype('f') + 1970
+
+    year = np.where(np.less(month, 3), year - 1, year)
+    month = np.where(np.less(month, 3), month + 12, month)
+
+    cond1 = np.less(year, 1582)
+    cond2 = np.logical_and(np.equal(year, 1582), np.greater(month, 10))
+    cond3 = np.logical_and(np.logical_and(np.equal(year, 1582), np.equal(month, 10)),
+                           np.greater(day, 15))
+
+    a__ = np.divide(year, 100).astype('i')
+    b__ = np.where(np.logical_or(np.logical_or(cond1, cond2), cond3),
+                   2 - a__ + a__ / 4, -10)
+
+    c__ = (365.25 * year).astype('i')
+    e__ = (30.6001 * (month + 1)).astype('i')
+    return b__ + c__ + e__ + day + 1720994.5
+
 
 def Julian(dobj):
     """Returns the number of julian days for the specified date-time.
@@ -36,21 +74,22 @@ def Julian(dobj):
 
     year = dobj.year
     month = dobj.month
-    day = dobj.day + (dobj.hour/24. + dobj.minute/(24*60.) + 
-                      dobj.second/(24*3600.) + 
-                      dobj.microsecond/(24*3600*1000000.))
+    day = dobj.day + (dobj.hour / 24. + dobj.minute / (24 * 60.) +
+                      dobj.second / (24 * 3600.) +
+                      dobj.microsecond / (24 * 3600 * 1000000.))
+
     if month < 3:
         year = year - 1
         month += 12
 
-    if (year > 1582 or 
-        (year == 1582 and month>10) or 
-        (year == 1582 and month==10 and day > 15)):
-        a__ = int(year/100)
+    if (year > 1582 or
+            (year == 1582 and month > 10) or
+            (year == 1582 and month == 10 and day > 15)):
+        a__ = int(year / 100)
         b__ = 2 - a__ + a__ / 4
-    
+
     c__ = int(365.25 * year)
-    e__ = int(30.6001 * (month+1))
+    e__ = int(30.6001 * (month + 1))
     return b__ + c__ + e__ + day + 1720994.5
 
 
@@ -61,15 +100,15 @@ def JulianToDate(jday):
     jday = jday + 0.5
     jdi = long(jday)
     if (jdi > 2299160):
-        a__ = long((jdi - 1867216.25)/36524.25)
-        b__ = long(jdi + 1 + a__ - a__/4)
+        a__ = long((jdi - 1867216.25) / 36524.25)
+        b__ = long(jdi + 1 + a__ - a__ / 4)
     else:
         b__ = jdi
 
     c__ = long(b__ + 1524)
-    d__ = long((c__ - 122.1)/365.25)
+    d__ = long((c__ - 122.1) / 365.25)
     e__ = long(365.25 * d__)
-    g__ = long((c__ - e__)/30.6001)
+    g__ = long((c__ - e__) / 30.6001)
     g1_ = long(30.6001 * g__)
 
     day = c__ - e__ - g1_
@@ -91,19 +130,20 @@ def JulianToDate(jday):
     microsec = int((seconds - int(seconds)) * 1000000)
     seconds = int(seconds)
 
-    return datetime.datetime(year, month, day, 
-                             hour=hour, minute=minutes, 
+    return datetime.datetime(year, month, day,
+                             hour=hour, minute=minutes,
                              second=seconds, microsecond=microsec)
+
 
 def sun_position(jday):
     """Get sun position"""
 
-    #double n,x,e,l,dl,v;
-    #double m2;
-    #int i;
+    # double n,x,e,l,dl,v;
+    # double m2;
+    # int i;
 
-    n__ = 360./365.2422 * jday
-    i__ = int(n__/360.)
+    n__ = 360. / 365.2422 * jday
+    i__ = int(n__ / 360.)
     n__ = n__ - i__ * 360.0
     x__ = n__ - 3.762863
     if x__ < 0:
@@ -116,28 +156,29 @@ def sun_position(jday):
         if fabs(dl_) < SMALL_FLOAT:
             break
 
-    v__ = 360./PI * arctan(1.01686011182 * tan(e__/2))
+    v__ = 360. / PI * arctan(1.01686011182 * tan(e__ / 2))
     sunpos = v__ + 282.596403
     i__ = int(sunpos / 360.)
     sunpos = sunpos - i__ * 360.0
 
     return sunpos
 
+
 def moon_position(jday, lsun):
     """Get the moon position"""
-    
+
     ms_ = 0.985647332099 * jday - 3.762863
     if ms_ < 0:
         ms_ = ms_ + 360.0
 
     mpos = 13.176396 * jday + 64.975464
-    i__ = int(mpos/360.0)
+    i__ = int(mpos / 360.0)
     mpos = mpos - i__ * 360.0
     if mpos < 0:
         mpos = mpos + 360.0
-    
+
     mm_ = mpos - 0.1114041 * jday - 349.383063
-    i__ = int(mm_/360.0)
+    i__ = int(mm_ / 360.0)
     mm_ = mm_ - i__ * 360.0
 
     ev_ = 1.2739 * sin(deg2rad(2 * (mpos - lsun) - mm_))
@@ -146,9 +187,10 @@ def moon_position(jday, lsun):
     mm_ = mm_ + ev_ - ae_ - 0.37 * sms
     ec_ = 6.2886 * sin(deg2rad(mm_))
     mpos = mpos + ev_ + ec_ - ae_ + 0.214 * sin(deg2rad(2 * mm_))
-    mpos = 0.6583 * sin(deg2rad(2*(mpos - lsun))) + mpos
+    mpos = 0.6583 * sin(deg2rad(2 * (mpos - lsun))) + mpos
 
     return mpos
+
 
 def moon_phase(dobj):
     """Calculate the phase of the moon"""
@@ -157,5 +199,4 @@ def moon_phase(dobj):
     lsun = sun_position(jday)
     lmoon = moon_position(jday, lsun)
 
-    return (1.0 - cos(deg2rad(lmoon - lsun)))/2.0
-
+    return (1.0 - cos(deg2rad(lmoon - lsun))) / 2.0

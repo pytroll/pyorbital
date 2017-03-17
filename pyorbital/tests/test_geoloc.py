c4f41aa2dbe3752c2187b20bf3c5c0034c020b62
Martin Raspaud <martin.raspaud@smhi.se>
2017-03-17 10:18:29 +0100

    Fix geoloc tests

--- pyorbital/tests/test_geoloc.py
+++ pyorbital/tests/test_geoloc.py
@@ -70,39 +70,52 @@ class TestGeoloc(unittest.TestCase):
     def test_scan_geometry(self):
         """Test the ScanGeometry object.
         """
-        instrument = ScanGeometry(np.deg2rad(np.array([[10, 0],
-                                                       [0, 0],
-                                                       [-10, 0]])),
-                                  np.array([-0.1, 0, 0.1]))
-        self.assertTrue(np.allclose(np.rad2deg(instrument.fovs[:, 0]),
-                                    np.array([10, 0, -10])))
+        scans_nb = 1
+
+        xy = np.vstack((np.deg2rad(np.array([10, 0, -10])),
+                        np.array([0, 0, 0])))
+        xy = np.tile(xy[:, np.newaxis, :], [1, np.int(scans_nb), 1])
+
+        times = np.tile([-0.1, 0, 0.1], [np.int(scans_nb), 1])
+
+        instrument = ScanGeometry(xy, times)
+
+        self.assertTrue(np.allclose(np.rad2deg(instrument.fovs[0]),
+                                    np.array([[10, 0, -10]])))
 
         # Test vectors
 
-        pos = np.array([[0, 0, 7000]]).T
-        vel = np.array([[1, 0, 0]]).T
+        pos = np.rollaxis(np.tile(np.array([0, 0, 7000]), [3, 1, 1]), 2)
+        vel = np.rollaxis(np.tile(np.array([1, 0, 0]), [3, 1, 1]), 2)
+        pos = np.stack([np.array([0, 0, 7000])] * 3, 1)[:, np.newaxis, :]
+        vel = np.stack([np.array([1, 0, 0])] * 3, 1)[:, np.newaxis, :]
+
         vec = instrument.vectors(pos, vel)
+
         self.assertTrue(np.allclose(np.array([[0, 0, -1]]),
-                                    vec[:, 1]))
+                                    vec[:, 0, 1]))
 
         # minus sin because we use trigonometrical direction of angles
 
         self.assertTrue(np.allclose(np.array([[0,
                                                -np.sin(np.deg2rad(10)),
                                                -np.cos(np.deg2rad(10))]]),
-                                    vec[:, 0]))
+                                    vec[:, 0, 0]))
         self.assertTrue(np.allclose(np.array([[0,
                                                -np.sin(np.deg2rad(-10)),
                                                -np.cos(np.deg2rad(-10))]]),
-                                    vec[:, 2]))
+                                    vec[:, 0, 2]))
 
         # Test times
 
         start_of_scan = np.datetime64(datetime(2014, 1, 8, 11, 30))
         times = instrument.times(start_of_scan)
-        self.assertEquals(times[1], start_of_scan)
-        self.assertEquals(times[0], start_of_scan - np.timedelta64(100, 'ms'))
-        self.assertEquals(times[2], start_of_scan + np.timedelta64(100, 'ms'))
+
+        self.assertEquals(times[0, 1], start_of_scan)
+        self.assertEquals(times[0, 0], start_of_scan -
+                          np.timedelta64(100, 'ms'))
+        self.assertEquals(times[0, 2], start_of_scan +
+                          np.timedelta64(100, 'ms'))
 
     def test_geodetic_lat(self):
         """Test the determination of the geodetic latitude.
@@ -126,21 +139,25 @@ class TestGeoloc(unittest.TestCase):
         b = 6356.75231414  # km, GRS80
         point = np.array([0, 0, 7000])
         nadir = subpoint(point, a, b)
-        self.assertTrue(np.allclose(nadir, np.array([[0, 0, b]]).T))
+        self.assertTrue(np.allclose(nadir, np.array([[0, 0, b]])))
 
         point = np.array([7000, 0, 7000])
         nadir = subpoint(point, a, b)
         self.assertTrue(np.allclose(nadir,
                                     np.array([[4507.85431429,
                                                0,
-                                               4497.06396339]]).T))
+                                               4497.06396339]])))
         points = np.array([[7000, 0, 7000],
                            [7000, 0, 7000]]).T
         nadir = subpoint(points, a, b)
-        self.assertTrue(np.allclose(nadir,
+        self.assertTrue(np.allclose(nadir[:, 0],
                                     np.array([[4507.85431429,
                                                0,
-                                               4497.06396339]]).T))
+                                               4497.06396339]])))
+        self.assertTrue(np.allclose(nadir[:, 1],
+                                    np.array([[4507.85431429,
+                                               0,
+                                               4497.06396339]])))
 
 
 class TestGeolocDefs(unittest.TestCase):
@@ -151,11 +168,11 @@ class TestGeolocDefs(unittest.TestCase):
         """Test the definition of the avhrr instrument
         """
         avh = avhrr(1, np.array([0, 1023.5, 2047]))
-        self.assertTrue(np.allclose(np.rad2deg(avh.fovs[:, 0]),
+        self.assertTrue(np.allclose(np.rad2deg(avh.fovs[0]),
                                     np.array([55.37, 0, -55.37])))
 
         avh = avhrr(1, np.array([0, 1023.5, 2047]), 10)
-        self.assertTrue(np.allclose(np.rad2deg(avh.fovs[:, 0]),
+        self.assertTrue(np.allclose(np.rad2deg(avh.fovs[0]),
                                     np.array([10, 0, -10])))
 
 

9b43860855cba66ffbf9c3b721be7c1513980bcf
stickler-ci <support@stickler-ci.com>
2018-10-16 14:00:42 +0000

    Fixing style errors.

--- pyorbital/__init__.py
+++ pyorbital/__init__.py
@@ -28,6 +28,7 @@ def dt2np(utc_time):
     except ValueError:
         return utc_time.astype('datetime64[ns]')
 
+
 from .version import get_versions
 __version__ = get_versions()['version']
 del get_versions

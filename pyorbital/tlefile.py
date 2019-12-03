d919e977b6c4ef9ab171defd7c71868f9280daf1
Evonosky <wevonosky@corp.arete.com>
2019-12-03 12:58:29 -0700

    Added name to authors list

--- pyorbital/tlefile.py
+++ pyorbital/tlefile.py
@@ -8,6 +8,7 @@
 #   Esben S. Nielsen <esn@dmi.dk>
 #   Martin Raspaud <martin.raspaud@smhi.se>
 #   Panu Lahtinen <panu.lahtinen@fmi.fi>
+#   Will Evonosky <william.evonosky@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

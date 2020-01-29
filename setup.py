8f4675f9b0455e29b4eff87ab6f1454337ce72f6
Panu Lahtinen <pnuu+git@iki.fi>
2020-01-29 10:49:45 +0200

    Install fetch_tles.py, fix pre-commit complaints

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Copyright (c) 2011-2014, 2018
-
+#
+# Copyright (c) 2011-2014, 2018, 2020
+#
 # Author(s):
-
+#
 #   Martin Raspaud <martin.raspaud@smhi.se>
-
+#   Panu Lahtinen <panu.lahtinen@fmi.fi>
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Setup for pyorbital."""
+
 from setuptools import setup
-import imp
 import versioneer
 
 setup(name='pyorbital',
@@ -42,6 +44,7 @@ setup(name='pyorbital',
       test_suite='pyorbital.tests.suite',
       package_dir={'pyorbital': 'pyorbital'},
       packages=['pyorbital'],
+      scripts=['bin/fetch_tles.py', ],
       install_requires=['numpy>=1.11.0,!=1.14.0', 'scipy'],
       zip_safe=False,
       )
